b97a1846b8d66b12d338187b9dc8113e68521dbb
Gladys Chan <gladiolus.c@gmail.com>
2014-12-08 06:47:38 -0500

    whoosh. fish are competing! game is almost a real game

--- server.js
+++ server.js
@@ -31,44 +31,84 @@ function requestHandler(req,res){
 
 // ==================== SOCKET.IO =====================
 var allFish = [];
-var appearance = [];
 var io = require('socket.io').listen(httpServer);
 
-var raceStarted = false;
+// GAME FLOW
+var GAME_WAIT = 0;
+var GAME_CANSTART = 1;
+var GAME_RUNNING = 2;
+var GAME_END = 3;
+var gameState = 99999999;
+
+var controllerLocked = false;
+var controller;
 
 io.sockets.on('connection',
   function(socket){
+    /*----------- Confirm identity from pool page--------*/
+    socket.on('iamcontroller',function(){
+      console.log("iamcontroller run");
+      if (controllerLocked == false){
+        console.log(allFish.length+" fish to be annihilated... :( ");
+        controllerLocked = true;
+        controller = socket;
+        //Game flow
+        gameState = GAME_WAIT;
+        socket.emit("newgamestate",GAME_WAIT);
+        //DISCONNECT ALL FISH AT START OF CONTROLLER
+        for (i=0;i<allFish.length;i++){
+          allFish[i].disconnect();
+        }
+      } else {
+        socket.emit("duplicateController");
+      }
+      // if pool page disconnects, all fish disconnect
+    });
+    /*---------------------------------------------------*/
 
-    socket.on('iamfish', function(){
+    /*----------- Confirm identity from fish page--------*/
+    socket.on('iamfish', function(){    
       console.log("i am a fish: "+socket.id+". "+allFish.length);
-      if (allFish.length < 4 && raceStarted == false){
-        console.log("server: sure you are fish");
-        allFish.push(socket);
+
+      if (allFish.length < 4 && controllerLocked && gameState == GAME_WAIT){
+        // Add to players array
         socket.totalDist = 0;
-        var red = Math.floor(Math.random()*255);
-        var green = Math.floor(Math.random()*255);
-        var blue = Math.floor(Math.random()*255);
+        allFish.push(socket);
+        console.log("server: sure you are fish. "+allFish.length);
+
+        /*check game state FOR SOME REASON IT"S NOT EMITTING EVEN THO IT"S RUNNING THIS SNIPPET D: D:
+        if (allFish.length == 4){
+          gameState = GAME_CANSTART;
+          console.log(gameState);          
+          socket.emit("newgamestate",gameState);
+        } */
+
+        // Assign color
+        var red = Math.floor(Math.random()*155)+100;
+        var green = Math.floor(Math.random()*155)+100;
+        var blue = Math.floor(Math.random()*155)+100;
         var color = "rgba("+red+","+green+","+blue+",1)";
-        console.log(color);
-        appearance.push(color);
         socket.emit("appearance",color); //fish.html
         socket.broadcast.emit("enterRace", color,socket.id); //pool.html
       } else {
         console.log("actually you are not");
         socket.emit("younotfish");
-      }
-
-      if (allFish.length == 4){
-        raceStarted = false;
-      }
+      }      
     });
+
+    socket.on("newgamestate", function(newGameState){
+      gameState = newGameState;
+      console.log("new game state received! "+newGameState);
+      console.log("allFish counts "+allFish.length+" fish");
+    });
+    /*----------------------------------------------------*/
 
     socket.on('move',
       function () {
         for (var i=0; i<allFish.length; i++){
-          if (allFish[i]===socket){
+          if (allFish[i]===socket && gameState == GAME_RUNNING){
             allFish[i].totalDist += 5;
-            console.log(i+" "+allFish[i].totalDist);
+            // console.log(i+" "+allFish[i].totalDist);
             socket.broadcast.emit("updateRace",allFish[i].totalDist,allFish[i].id);
             break; 
           }
@@ -76,9 +116,23 @@ io.sockets.on('connection',
       }
     );
 
+    socket.on('fishrank',function(socketid,rank){
+      for (var i=0;i<allFish.length;i++){
+        if (allFish[i].id == socketid){
+          allFish[i].emit('fishrankfish',rank);
+        }
+      }
+    });
+
 // ask Shawn: at disconnect how to know which fish to take out??
     socket.on('disconnect',
       function () {
+        console.log(socket.id+" disconnected");
+        // check if need another controller
+        if (controller==socket){
+          controllerLocked = false;
+        }
+
         for (var i=0; i<allFish.length; i++){
           if (allFish[i]===socket){
             // update fish count on server side.
